6ed1670e1836e70bf42282291ea023c7df63caec
gtheofilo <p16034@students.cs.unipi.gr>
2023-05-14 14:32:23 +0300

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -7,6 +7,26 @@ def _time_difference(timestamp1, timestamp2):
 def _calculate_speed(dis, time):
     return dis / time
     
+    
+def _haversine_np(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+
+    All args must be of equal length.
+
+    """
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = np.sin(dlat / 2.0) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2.0) ** 2
+
+    c = 2 * np.arcsin(np.sqrt(a))
+    km = 6367 * c
+    return km    
+    
 
 def calculate_bearing(*args, **kwargs):
     if len(args) == 0:
